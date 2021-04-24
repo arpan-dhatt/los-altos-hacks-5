a2a71728afc6fd08a8a2b68d4acda5f55a5c92e7
Arpan Dhatt <ary.dhatt@gmail.com>
2021-04-24 10:19:31 -0500

    Started iOS app

---
+++ first-responder-app/first-responder-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first-responder-app
+//
+//  Created by Arpan Dhatt on 4/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first-responder-app/first-responder-app/first_responder_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_responder_appApp.swift
+//  first-responder-app
+//
+//  Created by Arpan Dhatt on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct first_responder_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
