5427541bb9f3a4588261606045ebc708e4506f0a
Saaketh C <Saakethrchenna@gmail.com>
2021-04-25 06:21:08 -0500

    made ui better for the customer view

--- first-responder-app/first-responder-app/CustomerView.swift
+++ first-responder-app/first-responder-app/CustomerView.swift
@@ -18,11 +18,19 @@ struct CustomerView: View {
             Text("You Are Connected To The Disaster Duck Network").font(.title).bold().padding(.leading)
             RedButton()
             VStack(alignment: .leading){
-                Text("Your Current Location").padding(.vertical).font(.title3)
-                Text("Latitude: \(coordinate.latitude)")
-                Text("Longitude: \(coordinate.longitude)")
+                HStack{
+                    Text("Your Current Location").font(.title2).fontWeight(.light).padding(.vertical)
+                    Spacer()
+                }
+                HStack{
+                    Text("Latitude: \(coordinate.latitude)").font(.headline)
+                    Spacer()
+                }
+                HStack{
+                    Text("Longitude: \(coordinate.longitude)").font(.headline)
+                }.padding(.bottom, 25)
                 
-            }.padding().font(.headline)
+            }.padding().font(.headline).background(LinearGradient(gradient: Gradient(colors: [Color("red-light"),Color("red-dark")]), startPoint: .top, endPoint: .bottom)).cornerRadius(10).padding().padding(.horizontal).foregroundColor(.white)
         }
     }
 }
@@ -31,10 +39,10 @@ struct RedButton: View{
     @State var animate = false
     var body: some View {
         ZStack{
-            Circle().fill(Color.red.opacity(0.25)).frame(width: 375, height: 400).scaleEffect(self.animate ? 1 : 0)
-            Circle().fill(Color.red.opacity(0.35)).frame(width: 300, height: 350).scaleEffect(self.animate ? 1 : 0)
-            Circle().fill(Color.red.opacity(0.45)).frame(width: 225, height: 250).scaleEffect(self.animate ? 1 : 0)
-            Circle().fill(Color.red.opacity(1)).frame(width: 150, height: 150)
+            Circle().fill(Color("red-light").opacity(0.25)).frame(width: 375, height: 400).scaleEffect(self.animate ? 1 : 0)
+            Circle().fill(Color("red-light").opacity(0.35)).frame(width: 300, height: 350).scaleEffect(self.animate ? 1 : 0)
+            Circle().fill(Color("red-light").opacity(0.45)).frame(width: 225, height: 250).scaleEffect(self.animate ? 1 : 0)
+            Circle().fill(Color("red-light").opacity(1)).frame(width: 150, height: 150)
             Text("S.O.S").font(.title).foregroundColor(.white).fontWeight(.light)
         }.onTapGesture {
             self.animate.toggle()
