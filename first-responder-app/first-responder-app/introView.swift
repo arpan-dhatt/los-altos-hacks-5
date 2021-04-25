42f0de25c1471f7d13b74223863345bfdea4e604
Saaketh C <Saakethrchenna@gmail.com>
2021-04-24 19:47:42 -0500

    made intro page look good

--- first-responder-app/first-responder-app/introView.swift
+++ first-responder-app/first-responder-app/introView.swift
@@ -12,8 +12,19 @@ struct introView: View {
     
     var body: some View {
         ZStack{
+            
+            Circle().fill(Color("yellow")).frame(width: 100, height: 100).padding(.top, -243).padding(.leading, -224)
+            Circle().fill(Color("yellow")).frame(width: 125, height: 125).padding(.top, -320).padding(.leading, 320)
+            Circle().fill(Color("yellow")).frame(width: 75, height: 75).padding(.top, -400).padding(.leading, -150)
+            Circle().fill(Color("yellow")).frame(width: 100, height: 100).padding(.top, 750).padding(.leading, 200)
+            Circle().fill(Color("yellow")).frame(width: 50, height: 50).padding(.top, 650).padding(.leading, -125)
+            
             VStack(alignment: .center){
-                
+                HStack{
+                    Spacer()
+                    Text("Welcome To Disaster Duck").font(.title)
+                    Spacer()
+                }.padding(.top, 100)
                 Text("I Am").font(.largeTitle).bold().padding()
                 Spacer()
                 VStack{
@@ -22,7 +33,7 @@ struct introView: View {
                         Text("A First Responder").font(.title).fontWeight(.light)
                         Spacer()
                     }.padding().padding(.vertical,50)
-                }.background(Color.pink).cornerRadius(10).padding(.vertical).foregroundColor(.white).onTapGesture {
+                }.background(LinearGradient(gradient: Gradient(colors: [Color("blue-light"),Color("blue-dark")]), startPoint: .top, endPoint: .bottom)).cornerRadius(10).padding().padding(.horizontal).foregroundColor(.white).onTapGesture {
                     withAnimation{
                         viewModel.page = "responder"
                     }
@@ -34,7 +45,7 @@ struct introView: View {
                         Text("In Need Of Help").font(.title).fontWeight(.light)
                         Spacer()
                     }.padding().padding(.vertical,50)
-                }.background(Color.blue).cornerRadius(10).padding(.vertical).foregroundColor(.white).onTapGesture {
+                }.background(LinearGradient(gradient: Gradient(colors: [Color("red-light"),Color("red-dark")]), startPoint: .top, endPoint: .bottom)).cornerRadius(10).padding().padding(.horizontal).foregroundColor(.white).onTapGesture {
                     withAnimation{
                         viewModel.page = "customer"
                     }
