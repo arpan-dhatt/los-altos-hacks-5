576a54f8e6bcc898c323fe44c8fea4d88e59e7b9
Arpan Dhatt <ary.dhatt@gmail.com>
2021-04-24 21:11:44 -0500

    added network ingress from TCP socket to add floating point data to network

--- mesh-code/mesh-node/src/main.rs
+++ mesh-code/mesh-node/src/main.rs
@@ -1,12 +1,22 @@
-use std::sync::mpsc::{Sender, channel};
+use data_transcoder::*;
 use hci;
-use data_transcoder;
+use std::{io::Read, net::{TcpListener, TcpStream}, sync::mpsc::{channel, Sender}};
 
 fn main() {
     let lescan_thread = std::thread::spawn(hci::activate_lescan);
+
     let (packet_tx, packet_rx) = channel::<[u8; 40]>();
-    let packet_thread = std::thread::spawn(|| packet_thread_fn(packet_tx));
-    println!("{:?} {:?}", lescan_thread.join(), packet_thread.join());
+
+    let packet_thread_tx = packet_tx.clone();
+    let packet_thread = std::thread::spawn(|| packet_thread_fn(packet_thread_tx));
+
+    let graph_ingress_tx = packet_tx.clone();
+    let graph_ingress_thread = std::thread::spawn(|| graph_ingress_fn(graph_ingress_tx));
+
+    for packet in packet_rx {
+        println!("{}", String::from_utf8_lossy(&packet));
+    }
+    println!("{:?} {:?} {:?}", lescan_thread.join(), packet_thread.join(), graph_ingress_thread.join());
 }
 
 fn packet_thread_fn(tx: Sender<[u8; 40]>) {
@@ -14,7 +24,7 @@ fn packet_thread_fn(tx: Sender<[u8; 40]>) {
     for packet in scanner {
         if is_eddystone_packet(&packet) {
             if let Some(data) = extract_data(&packet) {
-                println!("{}", String::from_utf8_lossy(&data));
+                tx.send(data).unwrap();
             }
         }
     }
@@ -30,14 +40,51 @@ fn extract_data(packet: &str) -> Option<[u8; 40]> {
         let data_start = header_start + eddystone_header.len();
         let mut out = [b'0'; 40];
         for i in 0..40 {
-            if let Some(c) = packet.as_bytes().get(data_start+i) {
+            if let Some(c) = packet.as_bytes().get(data_start + i) {
                 out[i] = *c;
-            }
-            else {
-                return None
+            } else {
+                return None;
             }
         }
         return Some(out);
+    }
+    None
+}
+
+fn graph_ingress_fn(tx: Sender<[u8; 40]>) {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for connection in listener.incoming() {
+        if let Ok(stream) = connection {
+            if let Some((lat, long)) = handle_connection(stream) {
+                let mut advert_data = [b'0'; 40];
+                let lat_encoded = encode_discrete((lat / 0.00001) as i32);
+                let long_encoded = encode_discrete((long / 0.00001) as i32);
+                for i in 0..8 {
+                    advert_data[i] = lat_encoded[i];
+                    advert_data[i+8] = long_encoded[i];
+                }
+                tx.send(advert_data).unwrap();
+            }
+        }
+    }
+
+}
+
+fn handle_connection(mut stream: TcpStream) -> Option<(f32, f32)> {
+    let mut buffer = [0u8; 1024];
+    if stream.read(&mut buffer).is_ok() {
+        let stringified = String::from_utf8_lossy(&buffer);
+        let mut numbers = stringified
+            .split(" ")
+            .map(|e| e.parse::<f32>())
+            .filter_map(|e| match e {
+                Ok(n) => Some(n),
+                _ => None,
+            });
+        if let (Some(latitude), Some(longitude)) = (numbers.next(), numbers.next()) {
+            return Some((latitude, longitude))
+        }
     }
     None
 }
